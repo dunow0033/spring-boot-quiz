5eeb404c878483a98e1a3a41e2eb2967b75ed15c
Brian Donnoe <dunow33@hotmail.com>
2024-04-03 22:51:57 -0400

    added a little more

--- src/main/java/dao/QuestionRepository.java
+++ src/main/java/dao/QuestionRepository.java
@@ -3,9 +3,10 @@ package dao;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QuestionRepository {
+public interface QuestionRepository extends JpaRepository<Question, Integer>{
 	
 	public List<Question> findByCategory(String category);
 	
-	@Query()
+	@Query(value = "SELECT * FROM question q WHERE q.category=:category ORDER by RAND() LIMIT :numQ", nativeQuery = true)
+	public List<Question>
 }
